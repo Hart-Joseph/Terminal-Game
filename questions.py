79b22739b5df1564fa4c4e698055ac4c30e3a50b
Joseph Hart <joseph88hart@protonmail.com>
2024-11-24 00:29:23 -0600

    Create game cycle that updates a total score. Create questions list for the game.

---
+++ questions.py
@@ -0,0 +1,8 @@
+questions = [{
+    "Question": "Which of the following is a criteria that defines nursing as a profession?",
+    "A": "an undefined body of knowledge",
+    "B": "a dependence on the medical profession",
+    "C": "an ability to diagnose medical problems",
+    "D": "a profession that places patients in the center of care",
+    "Answer": "D"}]
+
--- script.py
+++ script.py
@@ -1,17 +1,47 @@
-class Player:
+from questions import questions
 
-    def __init__(self, name):
+class Player:
+    
+    def __init__(self, name = ""):
         self.name = name
         self.score = 0
+        self.total_score = 0
+
 
     def __repr__(self):
-        if self.score < 60:
-            print(f"{self.name} has a total score of {self.score} and received an F on this exam. Unfortunately, you did not pass. Study the subject material and better luck next time!")
-        elif self.score >= 60 and self.score<70:
-            print(f"{self.name} has a total score of {self.score} and received a D on this exam. Contratulations, you passed!")
-        elif self.score >= 70 and self.score < 80:
-            print(f"{self.name} has a total score of {self.score} and received a C on this exam. Contratulations, you passed!")
-        elif self.score >= 80 and self.score < 90:
-            print(f"{self.name} has a total score of {self.score} and received a B on this exam. Contratulations, you passed!")
+        if self.total_score < 60:
+            return f"Hey, {self.name}! You have a total score of {self.total_score}% and received an F on this exam. Unfortunately, you did not pass. Study the subject material and better luck next time!"
+        elif self.total_score >= 60 and self.total_score<70:
+            return f"Hey, {self.name}! You have a total score of {self.total_score}% and received a D on this exam. Contratulations, you passed!"
+        elif self.total_score >= 70 and self.total_score < 80:
+            return f"Hey, {self.name}! You have a total score of {self.total_score}% and received a C on this exam. Contratulations, you passed!"
+        elif self.total_score >= 80 and self.total_score < 90:
+            return f"Hey, {self.name}! You have a total score of {self.total_score}% and received a B on this exam. Contratulations, you passed!"
         else:
-            print(f"{self.name} has a total score of {self.score} and received an A on this exam. Contratulations, you passed!")
+            return f"Hey, {self.name}! You have a total score of {self.total_score}% and received an A on this exam. Contratulations, you passed!"
+        
+    def update_total_score(self):
+        self.total_score = (self.score / len(questions)) * 100
+
+
+player1 = Player()
+player1.name = input("Hey, there! Thank you for taking the time to sit down and take a nursing proficiency exam. May I ask you to please enter your name?\nName: ")
+print(f"It's a pleasure to meet you, {player1.name}. I hope that you will enjoy this test that is comprised of {len(questions)} questions.")
+for i in range(len(questions)):
+    print(questions[i].get("Question"))
+    print("A: " + questions[i].get("A"))
+    print("B: " + questions[i].get("B"))
+    print("C: " + questions[i].get("C"))
+    print("D: " + questions[i].get("D"))
+    player_answer = input("Enter your answer: ")
+    while player_answer.upper() not in questions[i]:
+        player_answer = input("That is not a valid entry. Please enter an answer from A to D: ")
+    if questions[i].get("Answer") == player_answer.upper():  # Directly compare with the answer letter
+        player1.score += 1
+        player1.update_total_score()
+        print("Correct!")
+    else:
+        print(f"The correct answer is {questions[i].get("Answer")}")
+
+
+print(player1)
