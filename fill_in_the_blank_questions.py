fd32afdcf3d1bd37e44be1501fbe98d753669a1c
Joseph Hart <joseph88hart@protonmail.com>
2024-11-28 01:49:05 -0600

    Create fill-in-the-blank questions list.

---
+++ fill_in_the_blank_questions.py
@@ -0,0 +1,23 @@
+fill_in_the_blank_questions = [
+    {
+        "Question": "Transfer of heat from a warm to a cool surface by direct contact is called _________.",
+        "Answers": ["conduction"]
+    },
+    {
+        "Question": "Core temperature below normal (less than 95F or 35C) is known as _________.",
+        "Answers": ["hypothermia"]
+    },
+    {
+        "Question": "Two factors that can impact pulse rate are _________ and _________.",
+        "Answers": ["developmental level", "gender", "exercise", "food intake", "stress", "fever", "disease", "blood loss", "position change", "medication"],
+        "Special": True
+    },
+    {
+        "Question": "The time of infection until manifestation of symptoms, a likely time to infect others is defined as the _________ stage.",
+        "Answers": ["incubation"]
+    },
+    {
+        "Question": "A state of cleanliness that decreases the potential for the spread of infections is called _________ asepsis.",
+        "Answers": ["medical"]
+    }
+]
